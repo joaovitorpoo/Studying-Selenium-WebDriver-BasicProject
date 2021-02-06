c973303d8d97b3a8c7c64dd38589b0cee32d34b8
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-02-06 20:08:24 -0300

    Test Insert A Future Movement

--- src/main/java/br/ce/wcaquino/test/MovimentacaoTest.java
+++ src/main/java/br/ce/wcaquino/test/MovimentacaoTest.java
@@ -1,12 +1,14 @@
 package br.ce.wcaquino.test;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import br.ce.wcaquino.core.BaseTest;
 import br.ce.wcaquino.page.MenuPage;
 import br.ce.wcaquino.page.MovimentacaoPage;
+import br.ce.wcaquino.util.DataUtils;
 
 public class MovimentacaoTest extends BaseTest {
 	private MenuPage menuPage = new MenuPage();
@@ -41,5 +43,26 @@ public class MovimentacaoTest extends BaseTest {
 				"Descrição é obrigatório", "Interessado é obrigatório", 
 				"Valor é obrigatório", "Valor deve ser um número")));
 		Assert.assertEquals(6, erros.size());
+	}
+	
+	@Test
+	public void testInserirMovimentacaoFutura(){
+		menuPage.acessarTelaInserirMovimentacao();
+		
+		Date dataFutura = DataUtils.obterDataComDiferencaDias(5);
+		
+		movPage.setDataMovimentacao(DataUtils.obterDataFormatada(dataFutura));
+		movPage.setDataPagamento(DataUtils.obterDataFormatada(dataFutura));
+		movPage.setDescricao("Movimentação do Teste");
+		movPage.setInteressado("Interessado Qualquer");
+		movPage.setValor("500");
+		movPage.setConta("Conta do Teste alterada");
+		movPage.setStatusPago();
+		movPage.salvar();
+		
+		List<String> erros = movPage.obterErros();
+		Assert.assertTrue(
+				erros.contains("Data da Movimentação deve ser menor ou igual à data atual"));
+		Assert.assertEquals(1, erros.size());
 	}
 }
