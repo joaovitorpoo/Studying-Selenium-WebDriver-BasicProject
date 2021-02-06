0541f43d3c4e832b26152596a77b15df6ac6ca0a
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-02-06 01:49:10 -0300

    Change Account Name

--- src/main/java/br/ce/wcaquino/core/BasePage.java
+++ src/main/java/br/ce/wcaquino/core/BasePage.java
@@ -173,4 +173,52 @@ public class BasePage {
 		JavascriptExecutor js = (JavascriptExecutor) getDriver();
 		return js.executeScript(cmd, param);
 	}
+	
+	/************** Tabela *********************/
+	
+	public WebElement obterCelula(String colunaBusca, String valor, String colunaBotao, String idTabela){
+		//procurar coluna do registro
+		WebElement tabela = getDriver().findElement(By.xpath("//*[@id='"+idTabela+"']"));
+		int idColuna = obterIndiceColuna(colunaBusca, tabela);
+		
+		//encontrar a linha do registro
+		int idLinha = obterIndiceLinha(valor, tabela, idColuna);
+		
+		//procurar coluna do botao
+		int idColunaBotao = obterIndiceColuna(colunaBotao, tabela);
+		
+		//clicar no botao da celula encontrada
+		WebElement celula = tabela.findElement(By.xpath(".//tr["+idLinha+"]/td["+idColunaBotao+"]"));
+		return celula;
+	}
+	
+	public void clicarBotaoTabela(String colunaBusca, String valor, String colunaBotao, String idTabela){
+		WebElement celula = obterCelula(colunaBusca, valor, colunaBotao, idTabela);
+		celula.findElement(By.xpath(".//input")).click();
+		
+	}
+
+	protected int obterIndiceLinha(String valor, WebElement tabela, int idColuna) {
+		List<WebElement> linhas = tabela.findElements(By.xpath("./tbody/tr/td["+idColuna+"]"));
+		int idLinha = -1;
+		for(int i = 0; i < linhas.size(); i++) {
+			if(linhas.get(i).getText().equals(valor)) {
+				idLinha = i+1;
+				break;
+			}
+		}
+		return idLinha;
+	}
+
+	protected int obterIndiceColuna(String coluna, WebElement tabela) {
+		List<WebElement> colunas = tabela.findElements(By.xpath(".//th"));
+		int idColuna = -1;
+		for(int i = 0; i < colunas.size(); i++) {
+			if(colunas.get(i).getText().equals(coluna)) {
+				idColuna = i+1;
+				break;
+			}
+		}
+		return idColuna;
+	}
 }
