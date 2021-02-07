4ba4087af7afcefafe38a7640bd2d7e960a2305f
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2021-02-07 12:39:13 -0300

    Check Empty Table

--- src/main/java/br/ce/wcaquino/test/ResumoTest.java
+++ src/main/java/br/ce/wcaquino/test/ResumoTest.java
@@ -2,8 +2,15 @@ package br.ce.wcaquino.test;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import static br.ce.wcaquino.core.DriverFactory.getDriver;
+
+import java.util.List;
+
 import br.ce.wcaquino.core.BaseTest;
+import br.ce.wcaquino.core.DriverFactory;
 import br.ce.wcaquino.page.MenuPage;
 import br.ce.wcaquino.page.ResumoPage;
 
@@ -25,6 +32,9 @@ public class ResumoTest extends BaseTest {
 		menuPage.acessarTelaResumo();
 		
 		Assert.assertEquals("Seu Barriga - Extrato", getDriver().getTitle());
+		
+		List<WebElement> elementosEncontrados =  DriverFactory.getDriver().findElements(By.xpath("//*[@id='tabelaExtrato']/tbody/tr"));
+		Assert.assertEquals(0, elementosEncontrados.size());
 	}
 	
 }
